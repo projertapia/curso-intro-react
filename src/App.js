6aaeb80837421d7a7adb8b7f679d8c7ceae29d4b
projertapia <projerdj@gmail.com>
2022-02-09 13:09:57 -0600

    Clase 6-22

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import React from "react";
+import {TodoCounter} from './TodoCounter';
+import {TodoSearch} from './TodoSearch';
+import {TodoList} from './TodoList';
+import {TodoItem} from './TodoItem';
+import {CreateTodoBotton} from './CreateTodoBotton';
+const todos= [
+  {text:'Cortar cebolla', completed: false},
+  {text:'tomar el curso de intro a React', completed: false},
+  {text:'Llorar con la llorona', completed: false},
+  {text:'lalalal', completed: false}
+]
 
-function App() {
+function App(props) {  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter/>
+      <TodoSearch/>
+        
+      <TodoList>
+        {todos.map(todo=>(
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+      </TodoList>
+
+      <CreateTodoBotton/>
+      
+    </React.Fragment>
   );
 }
 
---
+++ src/CreateTodoBotton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CreateTodoBotton(){
+    return(
+        <button>+</button>
+    )
+}
+
+export {CreateTodoBotton};

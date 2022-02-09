0f948776b69d2e84b1868040362f3572456191d5
projertapia <projerdj@gmail.com>
2022-02-09 17:50:58 -0600

    Clase 9-22

--- src/App.js
+++ src/App.js
@@ -5,21 +5,45 @@ import {TodoSearch} from './TodoSearch';
 import {TodoList} from './TodoList';
 import {TodoItem} from './TodoItem';
 import {CreateTodoBotton} from './CreateTodoBotton';
-const todos= [
+const defaultodos= [
   {text:'Cortar cebolla', completed: false},
   {text:'tomar el curso de intro a React', completed: true},
   {text:'Llorar con la llorona', completed: false},
   {text:'lalalal', completed: true}
 ]
 
-function App(props) {  
+function App(props) {
+  const[todos,setTodos] = React.useState(defaultodos);
+  const [searchValue, setSearchValue] = React.useState(''); 
+  
+  const completeTodos = todos.filter(todos => !!todos.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1){
+    searchedTodos = todos;
+  }else{
+    searchedTodos = todos.filter(todo =>{
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
   return (
     <React.Fragment>
-      <TodoCounter/>
-      <TodoSearch/>
+      <TodoCounter
+        total = {totalTodos}
+        completed= {completeTodos}
+      />
+      <TodoSearch
+        searchValue = {searchValue}
+        setSearchValue = {setSearchValue}
+      />
         
       <TodoList>
-        {todos.map(todo=>(
+        {searchedTodos.map(todo=>(
           <TodoItem
             key={todo.text}
             text={todo.text}
--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './TodoCounter.css';
 
-function TodoCounter(){
+function TodoCounter({total,completed}){
+    const promedioTodos = (completed/total)*100;
     return(
         <React.Fragment>
             <h2 className="TodoCounter">Mi lista <br/> de Tareas</h2>
             <span className="TodoCounterIndicator">
-                <span className="TodoCounterLevel" style={{width:'50%'}}></span>
-                <p className="TodoCounterText">50%</p>
+                <span className="TodoCounterLevel" style={{width:promedioTodos+'%'}}></span>
+                <p className="TodoCounterText">{promedioTodos}%</p>
             </span>
+            <p className="TodoCounterleyenda">{completed} de {total}</p>
         </React.Fragment>
     )
 }
